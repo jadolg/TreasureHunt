e98f34a5e7086dd395f9fe8646f6f27220f5116c
akiel <akiel@aleph.engineering>
2018-08-31 22:16:54 -0400

    Add method that generates the path to treasure

--- treasure_hunt_functional.py
+++ treasure_hunt_functional.py
@@ -1,1 +1,12 @@
 get_cell = lambda cell, array: array[int(cell[0]) - 1][int(cell[1]) - 1]
+
+
+def treasure_hunt(cell="11", visited=[], array=[[]]):
+    visited.append(cell)
+    if cell == get_cell(cell, array):
+        return visited
+    return treasure_hunt(
+        cell=get_cell(cell, array),
+        visited=visited,
+        array=array
+    )
