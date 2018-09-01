44c9356bb9112449f6051b1b6b53854c093b9b3c
akiel <akiel@aleph.engineering>
2018-08-31 21:11:43 -0400

    Add detection of loops in the path to define when there is no treasure

--- treasure_hunt_oo.py
+++ treasure_hunt_oo.py
@@ -3,6 +3,7 @@ class TreasureHuntOO(object):
     def __init__(self, array):
         self.current = None
         self.array = array
+        self.visited = []
 
     def __iter__(self):
         return self
@@ -11,9 +12,15 @@ class TreasureHuntOO(object):
         if self.current is None:
             self.current = "11"
         else:
-            if self.array[int(self.current[0]) - 1][int(self.current[1]) - 1] == self.current:
+            if self.current == "NO TREASURE" \
+                    or self.array[int(self.current[0]) - 1][int(self.current[1]) - 1] == self.current:
                 raise StopIteration
 
             self.current = self.array[int(self.current[0]) - 1][int(self.current[1]) - 1]
+
+        if self.current not in self.visited:
+            self.visited.append(self.current)
+        else:
+            self.current = "NO TREASURE"
 
         return self.current
