1205dd65e1195b214b64ac77178b084453eb6f78
akiel <akiel@aleph.engineering>
2018-08-31 21:10:08 -0400

    Add test to find if there is no treasure

--- tests.py
+++ tests.py
@@ -10,6 +10,16 @@ INPUT = [
     ["51", "23", "33", "54", "15"]
 ]
 
+# Cells 11 and 22 are in loop
+# If I detect a loop before finding a result it means there is no treasure
+INPUT_NO_TREASURE = [
+    ["22", "14", "25", "52", "21"],
+    ["44", "11", "11", "53", "43"],
+    ["24", "13", "45", "12", "34"],
+    ["42", "22", "43", "32", "41"],
+    ["51", "23", "33", "54", "15"]
+]
+
 OUTPUT = ["11", "55", "15", "21", "44", "32", "13", "25", "43"]
 
 
@@ -17,3 +27,8 @@ class TreasureHuntOOTestCase(unittest.TestCase):
     def test_hunt(self):
         treasure_hunt = TreasureHuntOO(array=INPUT)
         self.assertEqual(OUTPUT, list(treasure_hunt), "Output did not match the expected values")
+
+    def test_hunt_no_treasure(self):
+        treasure_hunt = list(TreasureHuntOO(array=INPUT_NO_TREASURE))
+        self.assertEqual("NO TREASURE", treasure_hunt[len(treasure_hunt) - 1],
+                         "Output did not match the expected values")
