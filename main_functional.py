8c625535048c4e4fd71fe29f1ad76a77e89f046b
akiel <akiel@aleph.engineering>
2018-08-31 22:51:39 -0400

    Print NO TREASURE if NoTreasureFoundException is raised

--- main_functional.py
+++ main_functional.py
@@ -1,7 +1,10 @@
+from exceptions import NoTreasureFoundException
 from treasure_hunt_functional import treasure_hunt
 
 if __name__ == '__main__':
     array = [input().split(" ") for i in range(5)]
-
-    for step in treasure_hunt(array=array):
-        print(step, end=" ")
+    try:
+        for step in treasure_hunt(array=array):
+            print(step, end=" ")
+    except NoTreasureFoundException:
+        print("NO TREASURE")
--- main_oo.py
+++ main_oo.py
@@ -1,7 +1,10 @@
+from exceptions import NoTreasureFoundException
 from treasure_hunt_oo import TreasureHunt
 
 if __name__ == '__main__':
     array = [input().split(" ") for i in range(5)]
-
-    for step in TreasureHunt(array=array):
-        print(step, end=" ")
+    try:
+        for step in TreasureHunt(array=array):
+            print(step, end=" ")
+    except NoTreasureFoundException:
+        print("NO TREASURE")
