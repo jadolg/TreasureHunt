e430cfd2b6e2b4e3b444bb258342b7ca087954a9
akiel <akiel@aleph.engineering>
2018-08-31 22:19:07 -0400

    Add condition to raise NoTreasureFoundException if it detects a loop

--- treasure_hunt_functional.py
+++ treasure_hunt_functional.py
@@ -1,7 +1,11 @@
+from exceptions import NoTreasureFoundException
+
 get_cell = lambda cell, array: array[int(cell[0]) - 1][int(cell[1]) - 1]
 
 
 def treasure_hunt(cell="11", visited=[], array=[[]]):
+    if cell in visited:
+        raise NoTreasureFoundException
     visited.append(cell)
     if cell == get_cell(cell, array):
         return visited
