5459822683cd35de251642be2d65865cb8ba9d1d
akiel <akiel@aleph.engineering>
2018-09-01 10:48:21 -0400

    Add functional solution using closure

--- treasure_hunt_functional.py
+++ treasure_hunt_functional.py
@@ -3,7 +3,9 @@ from exceptions import NoTreasureFoundException
 get_cell = lambda cell, array: array[int(cell[0]) - 1][int(cell[1]) - 1]
 
 
-def treasure_hunt_with_lambda(array, cell="11", visited=[]):
+def treasure_hunt_with_lambda(array, cell="11", visited=None):
+    if not visited:
+        visited = []
     if cell in visited:
         raise NoTreasureFoundException
     visited.append(cell)
@@ -14,3 +16,28 @@ def treasure_hunt_with_lambda(array, cell="11", visited=[]):
         visited=visited,
         array=array
     )
+
+
+def make_cell_getter(array):
+    def next_cell(cell):
+        if cell == array[int(cell[0]) - 1][int(cell[1]) - 1]:
+            return None
+        else:
+            return array[int(cell[0]) - 1][int(cell[1]) - 1]
+
+    return next_cell
+
+
+def treasure_hunt_with_closure(array, cell='11', visited=None):
+    if not visited:
+        visited = []
+    get_cell_f = make_cell_getter(array)
+    if cell not in visited:
+        visited.append(cell)
+        cell = get_cell_f(cell)
+        if not cell:
+            return visited
+        else:
+            return treasure_hunt_with_closure(array, cell, visited)
+    else:
+        raise NoTreasureFoundException
